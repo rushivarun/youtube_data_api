a0ebf1d39b374419d7f49633f0fe8ee65214fc8b
Rushi Varun <varun.muniaplle2017@vitstudent.ac.in>
2021-03-05 10:11:03 +0530

    bind address from mysql updated

--- app/mysql-conf/files/create_table.sql
+++ app/mysql-conf/files/create_table.sql
@@ -2,6 +2,6 @@ CREATE DATABASE yt_api;
 
 USE yt_api;
 
-CREATE TABLE Football (title TEXT(200), description TEXT(2000), publishedAt TEXT(50), VID INT PRIMARY KEY);
+CREATE TABLE Football (title TEXT(200), description TEXT(2000), publishedAt TEXT(50), VID INT AUTO_INCREMENT PRIMARY KEY);
 
 
